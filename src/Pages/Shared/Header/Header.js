a60db0ac115fe0653b0d4c2313f39819a159cf44
MdNaimRipto <naimurtsc567@gmail.com>
2022-11-08 09:39:44 +0600

    navbar completed

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,93 @@
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <Navbar
+            className='py-6 border-b border-gray-300 container mx-auto'
+            fluid={true}
+            rounded={true}
+        >
+            <Navbar.Brand>
+                <img
+                    src="https://flowbite.com/docs/images/logo.svg"
+                    className="mr-3 h-6 sm:h-9"
+                    alt="Flowbite Logo"
+                />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                    Flowbite
+                </span>
+            </Navbar.Brand>
+            <div className="flex flex-row-reverse md:flex-row justify-between md:order-2 w-1/4 md:w-[35%]">
+                <Navbar.Collapse className='hidden md:flex items-center mr-8'>
+                    <Navbar.Link
+                        href="/navbars"
+                    >
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        About
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Services
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Pricing
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+                <Dropdown
+                    arrowIcon={false}
+                    inline={true}
+                    label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+                >
+                    <Dropdown.Header>
+                        <span className="block text-sm">
+                            Bonnie Green
+                        </span>
+                        <span className="block truncate text-sm font-medium">
+                            name@flowbite.com
+                        </span>
+                    </Dropdown.Header>
+                    <Dropdown.Item>
+                        Dashboard
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                        Settings
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                        Earnings
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item>
+                        Sign out
+                    </Dropdown.Item>
+                </Dropdown>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse className='block md:hidden'>
+                <Navbar.Link
+                    href="/navbars"
+                >
+                    Home
+                </Navbar.Link>
+                <Navbar.Link href="/navbars">
+                    About
+                </Navbar.Link>
+                <Navbar.Link href="/navbars">
+                    Services
+                </Navbar.Link>
+                <Navbar.Link href="/navbars">
+                    Pricing
+                </Navbar.Link>
+                <Navbar.Link href="/navbars">
+                    Contact
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default Header;
